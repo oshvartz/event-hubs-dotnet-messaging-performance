1dfb388a1b8defdb2bce0a7ebe92e3867e19b6ce
Offir Shvartz <ofshvart@microsoft.com>
2023-11-08 13:42:42 +0200

    update nuget

--- src/ThroughputTest/ProcessorTask.cs
+++ src/ThroughputTest/ProcessorTask.cs
@@ -31,7 +31,7 @@ namespace ThroughputTest
                                 options.EventHubName,
                                 options.ProcessingTimeDurationMs,
                                 //we want to masure latecy so we are reading from the last event
-                                new Azure.Messaging.EventHubs.Primitives.EventProcessorOptions { DefaultStartingPosition = EventPosition.Earliest, MaximumWaitTime = TimeSpan.FromMinutes(100), PrefetchCount = options.PrefetchCount })).ToList();
+                                new Azure.Messaging.EventHubs.Primitives.EventProcessorOptions { DefaultStartingPosition = EventPosition.Latest, PrefetchCount = options.PrefetchCount })).ToList();
         }
 
         public async override Task ExecuteAsync(CancellationToken cancellationToken)
